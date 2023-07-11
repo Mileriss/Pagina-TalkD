ba8166c20ac99211b08edad7d7bf45f79f537b86
Rafael Mileris <rafaelmileris15@gmail.com>
2023-07-11 20:34:34 -0300

    Arquivo Javascript para criar o ScrollReveal

---
+++ script.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal({reset: true}) //Declaração da API
+
+sr.reveal('.titulos', {duration: 1000}) //Declaração da classe e a duração do efeito
+sr.reveal('.sub-titulos', {duration: 2000})
+sr.reveal('.textos', {duration: 3000})
