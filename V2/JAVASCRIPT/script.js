912f62b60ddeb977a54235c606673b343f459c14
Rafael Mileris <rafaelmileris15@gmail.com>
2023-08-30 20:27:12 -0300

    Adicionando as classes no Scrollreveal (Basicão - é +)

--- V2/JAVASCRIPT/script.js
+++ V2/JAVASCRIPT/script.js
@@ -1,5 +1,21 @@
 window.sr = ScrollReveal({reset: true}) //Declaração da API
 
-sr.reveal('.titulos', {duration: 1000}) //Declaração da classe e a duração do efeito
-sr.reveal('.sub-titulos', {duration: 2000})
-sr.reveal('.textos', {duration: 3000})
+// TITULOS E SUBTITULOS
+sr.reveal('.titulo-principal', {duration: 1500}) //Declaração da classe e a duração do efeito
+sr.reveal('.sub-titulo', {duration: 2000})
+
+// TEXTOS
+// Principal
+sr.reveal('.txt-principal', {
+    rotate: {x: 0, y: 20, z: 0},
+    duration: 1500
+})
+
+//Listas
+sr.reveal('.txt-listas',{duration: 1500})
+
+//Grids
+sr.reveal('.txt-grid', {duration: 1500})
+
+//BACKGROUNDS
+sr.reveal('.bloco-background', {duration: 1000})
